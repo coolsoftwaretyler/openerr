845c8fb00da17360a43f7d06e038cd5b5552e31a
Tyler Williams <tyler@ogdenstudios.xyz>
2019-04-28 17:58:21 -0600

    get CO bill swith actionSince a specific date

--- openerr.js
+++ openerr.js
@@ -2,16 +2,23 @@
 const axios = require('axios');
 const secrets = require("./secrets.json");
 const url = "https://openstates.org/graphql";
+var today = new Date();
 
-var osq = '{search: bills(first: 100, jurisdiction: "Colorado") {edges {node {id}}}}'
+// For testing purposes. TODO: make sure we use the correct date
+var date = today.getFullYear()+'-0'+(today.getMonth()+1)+'-'+(today.getDate()-8);
+
+// TODO: use this one for the date.
+// var date = today.getFullYear()+'-0'+(today.getMonth()+1)+'-'+today.getDate();
+
+var openStatesQuery = '{search: bills(first: 100, jurisdiction: "Colorado", actionSince: "'+ date +'") {edges {node {id, title}}}}'
+
 // Set the API Key header for all requests
 axios.defaults.headers.common['X-API-KEY'] = secrets.openStatesKey;
 
 async function getIt() {
     try {
-        const response = await axios.get(url, {params: { query: osq }});
-        console.log("success!");
-        console.log(response.data);
+        const response = await axios.get(url, {params: { query: openStatesQuery }});
+        process(response.data);
     } catch (error) {
         console.log("error :(");
         console.error(error);
@@ -19,3 +26,11 @@ async function getIt() {
 }
 
 getIt();
+
+function process(data){
+    responseData = data.data.search.edges;
+    console.log(responseData.length);
+    for (i=0;i<responseData.length;i++) {
+        console.log(responseData[i]);
+    }
+}
