071b73702e334ed3d4aecbd354ebf4efe51485e1
Tyler Williams <tyler@ogdenstudios.xyz>
2019-04-28 17:47:41 -0600

    successfully get 100 CO bills with AXIOS

--- openerr.js
+++ openerr.js
@@ -1,18 +1,21 @@
-console.log("Openerr");
-//var url = 'https://openstates.org/graphql'
-// headers = { "X-API-KEY" => Rails.application.credentials.open_states_api_key, "Content-type" => "application/json" }
-// body = {query: "{bill(id : \"#{self.bill_id}\") { updatedAt }}" }.to_json
+// Load dependencies and set up constants 
+const axios = require('axios');
+const secrets = require("./secrets.json");
+const url = "https://openstates.org/graphql";
 
-const https = require("https");
-const url = "https://jsonplaceholder.typicode.com/posts/1";
-https.get(url, res => {
-  res.setEncoding("utf8");
-  let body = "";
-  res.on("data", data => {
-    body += data;
-  });
-  res.on("end", () => {
-    body = JSON.parse(body);
-    console.log(body);
-  });
-});
+var osq = '{search: bills(first: 100, jurisdiction: "Colorado") {edges {node {id}}}}'
+// Set the API Key header for all requests
+axios.defaults.headers.common['X-API-KEY'] = secrets.openStatesKey;
+
+async function getIt() {
+    try {
+        const response = await axios.get(url, {params: { query: osq }});
+        console.log("success!");
+        console.log(response.data);
+    } catch (error) {
+        console.log("error :(");
+        console.error(error);
+    }
+}
+
+getIt();
