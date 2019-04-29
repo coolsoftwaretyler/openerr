a753730c266db1334cdd6840ce62c687cb372ab3
Tyler Williams <tyler@ogdenstudios.xyz>
2019-04-28 21:57:11 -0600

    everything except twitter functionality is p much working

--- openerr.js
+++ openerr.js
@@ -7,33 +7,33 @@ const url = "https://openstates.org/graphql";
 var today = new Date();
 bills = []
 // For testing purposes. TODO: make sure we use the correct date
-var date = today.getFullYear() + '-0' + (today.getMonth() + 1) + '-' + (today.getDate() - 8);
+//var date = today.getFullYear() + '-0' + (today.getMonth() + 1) + '-' + (today.getDate() - 8);
 // TODO: use this one for the date.
-// var date = today.getFullYear()+'-0'+(today.getMonth()+1)+'-'+today.getDate();
+var date = today.getFullYear() + '-0' + (today.getMonth() + 1) + '-' + today.getDate();
 var openStatesQuery = createOpenStatesQuery(date);
 // Set the API Key header for all requests
 axios.defaults.headers.common['X-API-KEY'] = secrets.openStatesKey;
 
-// Check how many bills meet the criteria 
 function getIt(url, query) {
     axios.get(url, { params: { query: query } })
         .then(function (response) {
             var hasNextPage = response.data.data.search.pageInfo.hasNextPage;
             var endCursor = response.data.data.search.pageInfo.endCursor;
-            if (hasNextPage){
-                console.log("there's another page at " + endCursor.toString());
+            if (hasNextPage) {
                 var responseData = response.data.data.search.edges;
-                for (i=0; i<responseData.length; i++) {
-                    bills.push(responseData[i]);
+                for (i = 0;i < responseData.length;i++) {
+                    bill = constructBillObject(responseData[i]);
+                    bills.push(bill);
                 }
                 var newQuery = createOpenStatesQuery(date, endCursor);
                 getIt(url, newQuery);
             } else {
-                console.log("no more new pages");
-                var responseData = data.data.search.edges;
-                for (i=0; i<responseData.length; i++) {
-                    bills.push(responseData[i]);
+                var responseData = response.data.data.search.edges;
+                for (i = 0;i < responseData.length;i++) {
+                    bill = constructBillObject(responseData[i]);
+                    bills.push(bill);
                 }
+                tweet();
             }
         })
         .catch(function (error) {
@@ -43,11 +43,20 @@ function getIt(url, query) {
 
 getIt(url, openStatesQuery);
 
-// Tweet about it 
-// TODO: tweet logic to avoid ratelimiting
+// Tweet out the results 
+function tweet() {
+    if (bills.length === 0) {
+        console.log("No bills found");
+    } else {
+        for (i = 0;i < bills.length;i++) {
+            var readMore = bills[i].openstatesURL ? "Read more at: " + bills[i].openstatesURL.toString() : '';
+            var tweetText = `Colorado ${bills[i].identifier}: ${bills[i].title}. On ${bills[i].latestActionDate}, the following action was taken: ${bills[i].latestAction}. ${readMore}`;
+            console.log(tweetText);
+        }
+    }
+}
 
 // Open States Query constructor 
-
 function createOpenStatesQuery(date, cursor = null) {
     var query = `
         { 
@@ -56,6 +65,8 @@ function createOpenStatesQuery(date, cursor = null) {
                     node {
                         id,
                         title,
+                        identifier,
+                        openstatesUrl,
                         actions {
                             description
                             date
@@ -72,4 +83,16 @@ function createOpenStatesQuery(date, cursor = null) {
         }
     `;
     return query;
+}
+
+// Bill object constructor 
+function constructBillObject(data) {
+    bill = {
+        identifier: data.node.identifier,
+        title: data.node.title,
+        latestAction: data.node.actions[0].description,
+        latestActionDate: data.node.actions[0].date.split("T")[0],
+        openstatesURL: data.node.openstatesUrl
+    }
+    return bill;
 }
