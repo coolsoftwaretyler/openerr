bbeea9c3a4cfceb9436f2e71a8b865f43f147961
Tyler Williams <tyler@ogdenstudios.xyz>
2019-04-28 21:11:06 -0600

    add a helper function for making open states queries

--- openerr.js
+++ openerr.js
@@ -5,62 +5,71 @@ const secrets = require("./secrets.json");
 // Set up constants and variables 
 const url = "https://openstates.org/graphql";
 var today = new Date();
+bills = []
 // For testing purposes. TODO: make sure we use the correct date
 var date = today.getFullYear() + '-0' + (today.getMonth() + 1) + '-' + (today.getDate() - 8);
 // TODO: use this one for the date.
 // var date = today.getFullYear()+'-0'+(today.getMonth()+1)+'-'+today.getDate();
-var openStatesQuery = '{search: bills(first: 100, jurisdiction: "Colorado", actionSince: "' + date + '") {totalCount, edges {node {id, title, actions{description, date}}}}}'
-
+var openStatesQuery = createOpenStatesQuery(date);
 // Set the API Key header for all requests
 axios.defaults.headers.common['X-API-KEY'] = secrets.openStatesKey;
 
 // Check how many bills meet the criteria 
-function getTotalCount() {
-    axios.get(url, { params: { query: openStatesQuery } })
+function getIt(url, query) {
+    axios.get(url, { params: { query: query } })
         .then(function (response) {
-            totalCount = response.data.data.search.totalCount;
-            pageResults(totalCount, response);
+            var hasNextPage = response.data.data.search.pageInfo.hasNextPage;
+            var endCursor = response.data.data.search.pageInfo.endCursor;
+            if (hasNextPage){
+                console.log("there's another page at " + endCursor.toString());
+                var responseData = response.data.data.search.edges;
+                for (i=0; i<responseData.length; i++) {
+                    bills.push(responseData[i]);
+                }
+                var newQuery = createOpenStatesQuery(date, endCursor);
+                getIt(url, newQuery);
+            } else {
+                console.log("no more new pages");
+                var responseData = data.data.search.edges;
+                for (i=0; i<responseData.length; i++) {
+                    bills.push(responseData[i]);
+                }
+            }
         })
         .catch(function (error) {
             console.log(error);
         });
 }
 
-function pageResults(count, response){
-    // Set up an object to hold those bills 
-    var bills = {};
-    // Page through all the bills that meet the criteria 
-    // Push the title, bill identifier, open states ID, action title and action date into the object
-    console.log(count);
-    console.log(response);
-}
-
-getTotalCount();
+getIt(url, openStatesQuery);
 
 // Tweet about it 
 // TODO: tweet logic to avoid ratelimiting
 
-async function getIt() {
-    try {
-        const response = await axios.get(url, { params: { query: openStatesQuery } });
-        process(response.data);
-    } catch (error) {
-        console.error(error);
-    }
-}
+// Open States Query constructor 
 
-//getIt();
-
-function process(data) {
-    totalCount = data.data.search.totalCount;
-    console.log(totalCount);
-    responseData = data.data.search.edges;
-    for (i = 0;i < responseData.length;i++) {
-        var title = responseData[i].node.title;
-        // console.log(title);
-        var actions = responseData[i].node.actions;
-        for (j = 0;j < actions.length;j++) {
-            // console.log(actions[j]);
+function createOpenStatesQuery(date, cursor = null) {
+    var query = `
+        { 
+            search: bills(first:100, after:"${cursor ? cursor : ''}", jurisdiction: "Colorado", actionSince: "${date}") {
+                edges {
+                    node {
+                        id,
+                        title,
+                        actions {
+                            description
+                            date
+                        }
+                    }
+                }
+                pageInfo {
+                    hasNextPage
+                    hasPreviousPage
+                    endCursor
+                    startCursor
+                }
+            }
         }
-    }
+    `;
+    return query;
 }
