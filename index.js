f831b6bff158c43d1d281b6718b793d78705220e
Tyler Williams <tyler@ogdenstudios.xyz>
2019-05-02 10:03:13 -0600

    add prettier, run it and eslint

--- index.js
+++ index.js
@@ -1,84 +1,94 @@
 // Load dependencies
 const axios = require('axios');
-const secrets = require("./secrets.json");
+const secrets = require('./secrets.json');
 var Twitter = require('twitter');
 // Set up constants and variables
-const url = "https://openstates.org/graphql";
+const url = 'https://openstates.org/graphql';
 var env = 'test';
 var twitter = new Twitter({
-    consumer_key: secrets.api_key,
-    consumer_secret: secrets.api_secret_key,
-    access_token_key: secrets.access_token,
-    access_token_secret: secrets.access_token_secret
+  consumer_key: secrets.api_key,
+  consumer_secret: secrets.api_secret_key,
+  access_token_key: secrets.access_token,
+  access_token_secret: secrets.access_token_secret,
 });
 var d = new Date();
 d.setDate(d.getDate() - 1);
 var date = d.toISOString().split('T')[0];
 var openStatesQuery = createOpenStatesQuery(date);
 function getIt(url, query, bills) {
-    axios.get(url, { params: { query: query }, headers: { 'X-API-KEY': secrets.openStatesKey } })
-        .then(function (response) {
-            var hasNextPage = response.data.data.search.pageInfo.hasNextPage;
-            var endCursor = response.data.data.search.pageInfo.endCursor;
-            if (hasNextPage) {
-                var responseData = response.data.data.search.edges;
-                for (i = 0;i < responseData.length;i++) {
-                    bill = createBillObject(responseData[i]);
-                    bills.push(bill);
-                }
-                var newQuery = createOpenStatesQuery(date, endCursor);
-                getIt(url, newQuery, bills);
-            } else {
-                var responseData = response.data.data.search.edges;
-                for (i = 0;i < responseData.length;i++) {
-                    bill = createBillObject(responseData[i]);
-                    bills.push(bill);
-                }
-                startTweeting(bills);
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+  axios
+    .get(url, {
+      params: {query: query},
+      headers: {'X-API-KEY': secrets.openStatesKey},
+    })
+    .then(function(response) {
+      var hasNextPage = response.data.data.search.pageInfo.hasNextPage;
+      var endCursor = response.data.data.search.pageInfo.endCursor;
+      var responseData = response.data.data.search.edges;
+      if (hasNextPage) {
+        for (i = 0; i < responseData.length; i++) {
+          bill = createBillObject(responseData[i]);
+          bills.push(bill);
+        }
+        var newQuery = createOpenStatesQuery(date, endCursor);
+        getIt(url, newQuery, bills);
+      } else {
+        for (i = 0; i < responseData.length; i++) {
+          bill = createBillObject(responseData[i]);
+          bills.push(bill);
+        }
+        startTweeting(bills);
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
 
 // Tweet out the results
 function startTweeting(bills, testing = false) {
-    if (bills.length === 0) {
-        console.log('No bills found today');
-        return false;
-    } else {
-        for (i = 0;i < bills.length;i++) {
-            var readMore = bills[i].openstatesUrl ? "Read more at: " + bills[i].openstatesUrl.toString() : '';
-            var tweetText = `Colorado ${bills[i].identifier}: ${bills[i].title}. On ${bills[i].latestActionDate}, the following action was taken: ${bills[i].latestAction}. ${readMore}`;
-            if (testing) {
-                return bills;
-            } else {
-                tweet(tweetText);
-            }
-        }
+  if (bills.length === 0) {
+    console.log('No bills found today');
+    return false;
+  } else {
+    for (i = 0; i < bills.length; i++) {
+      var readMore = bills[i].openstatesUrl
+        ? 'Read more at: ' + bills[i].openstatesUrl.toString()
+        : '';
+      var tweetText = `Colorado ${bills[i].identifier}: ${bills[i].title}. On ${
+        bills[i].latestActionDate
+      }, the following action was taken: ${bills[i].latestAction}. ${readMore}`;
+      if (testing) {
+        return bills;
+      } else {
+        tweet(tweetText);
+      }
     }
+  }
 }
 
 function tweet(status) {
-    if (env === 'production') {
-        twitter.post('statuses/update', { status: status })
-            .then(function (tweet) {
-                console.log(tweet);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    } else {
-        console.log(status);
-    }
+  if (env === 'production') {
+    twitter
+      .post('statuses/update', {status: status})
+      .then(function(tweet) {
+        console.log(tweet);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  } else {
+    console.log(status);
+  }
 }
 
 // Open States Query constructor
 function createOpenStatesQuery(date, cursor = null) {
-    var query = `
+  var query = `
         {
-            search: bills(first:100, after:"${cursor ? cursor : ''}", jurisdiction: "Colorado", actionSince: "${date}") {
+            search: bills(first:100, after:"${
+              cursor ? cursor : ''
+            }", jurisdiction: "Colorado", actionSince: "${date}") {
                 edges {
                     node {
                         id,
@@ -100,48 +110,48 @@ function createOpenStatesQuery(date, cursor = null) {
             }
         }
     `;
-    return query;
+  return query;
 }
 
 // Bill object constructor
 function createBillObject(data) {
-    bill = {
-        identifier: data.node.identifier,
-        title: data.node.title,
-        latestAction: data.node.actions[0].description,
-        latestActionDate: data.node.actions[0].date.split("T")[0],
-        openstatesUrl: data.node.openstatesUrl
-    };
-    return bill;
+  bill = {
+    identifier: data.node.identifier,
+    title: data.node.title,
+    latestAction: data.node.actions[0].description,
+    latestActionDate: data.node.actions[0].date.split('T')[0],
+    openstatesUrl: data.node.openstatesUrl,
+  };
+  return bill;
 }
 
 // AWS Lambda handler
-exports.handler = function (event, context, callback) {
-    console.log(event);
-    console.log(context);
-    env = 'production';
-    getIt(url, openStatesQuery, []);
-    callback(null, "Success from lambda");
+exports.handler = function(event, context, callback) {
+  console.log(event);
+  console.log(context);
+  env = 'production';
+  getIt(url, openStatesQuery, []);
+  callback(null, 'Success from lambda');
 };
 
 // Test exports
 
 // Check that open states queries get created correctly
-exports.testCreateOpenStatesQuery = function (date, cursor) {
-    return createOpenStatesQuery(date, cursor).raw;
+exports.testCreateOpenStatesQuery = function(date, cursor) {
+  return createOpenStatesQuery(date, cursor).raw;
 };
 
 // Check that bill objects get created correctly
-exports.testCreateBillObject = function (data) {
-    return createBillObject(data);
+exports.testCreateBillObject = function(data) {
+  return createBillObject(data);
 };
 
 // Check that startTweeting works
-exports.testStartTweeting = function (bills) {
-    return startTweeting(bills, true);
+exports.testStartTweeting = function(bills) {
+  return startTweeting(bills, true);
 };
 
 // If we said to run it local, run it.
 if (process.argv[2] === 'local') {
-    getIt(url, openStatesQuery, []);
+  getIt(url, openStatesQuery, []);
 }
--- test/test.js
+++ test/test.js
@@ -25,50 +25,68 @@ var correctOpenStatesQuery = `
 }
 `;
 var sampleBillData = {
-    node : {
-        identifier: 'xxx-xxx-xxx',
-        title: 'Correct bill object',
-        actions : [
-            {
-                description: 'Latest action taken',
-                date: '2019-04-20T04:20:00'
-            }
-        ],
-        openstatesUrl: 'https://openstates.org'
-    }
+  node: {
+    identifier: 'xxx-xxx-xxx',
+    title: 'Correct bill object',
+    actions: [
+      {
+        description: 'Latest action taken',
+        date: '2019-04-20T04:20:00',
+      },
+    ],
+    openstatesUrl: 'https://openstates.org',
+  },
 };
 
 var correctBillObject = {
-    identifier: 'xxx-xxx-xxx',
-    title: 'Correct bill object',
-    latestAction: 'Latest action taken',
-    latestActionDate: '2019-04-20',
-    openstatesUrl: 'https://openstates.org'
+  identifier: 'xxx-xxx-xxx',
+  title: 'Correct bill object',
+  latestAction: 'Latest action taken',
+  latestActionDate: '2019-04-20',
+  openstatesUrl: 'https://openstates.org',
 };
 
 describe('Openerr', function() {
-    describe('#createOpenStatesQuery()', function() {
-        it('should create the Open States query as expected', function() {
-            // Use `.raw` to compare raw strings.
-          assert.equal(openerr.testCreateOpenStatesQuery('2019-04-20', 'xxx6969xxx'), correctOpenStatesQuery.raw);
-        });
-      });
-      describe('#createBillObject()', function() {
-        it('should create bill objects as expected', function() {
-          assert.equal(openerr.testCreateBillObject(sampleBillData).identifier, correctBillObject.identifier);
-          assert.equal(openerr.testCreateBillObject(sampleBillData).title, correctBillObject.title);
-          assert.equal(openerr.testCreateBillObject(sampleBillData).latestAction, correctBillObject.latestAction);
-          assert.equal(openerr.testCreateBillObject(sampleBillData).latestActionDate, correctBillObject.latestActionDate);
-          assert.equal(openerr.testCreateBillObject(sampleBillData).openstatesUrl, correctBillObject.openstatesUrl);
-        });
-      });
-      describe('#startTweeting()', function() {
-        it('should return false when bills are empty', function() {
-            // We should receive an email about this as well
-            assert.equal(openerr.testStartTweeting([]), false);
-        });
-        it('should return the bills when they are present', function() {
-            assert.equal(openerr.testStartTweeting([correctBillObject]).length, 1);
-        });
-      });
+  describe('#createOpenStatesQuery()', function() {
+    it('should create the Open States query as expected', function() {
+      // Use `.raw` to compare raw strings.
+      assert.equal(
+        openerr.testCreateOpenStatesQuery('2019-04-20', 'xxx6969xxx'),
+        correctOpenStatesQuery.raw
+      );
+    });
   });
+  describe('#createBillObject()', function() {
+    it('should create bill objects as expected', function() {
+      assert.equal(
+        openerr.testCreateBillObject(sampleBillData).identifier,
+        correctBillObject.identifier
+      );
+      assert.equal(
+        openerr.testCreateBillObject(sampleBillData).title,
+        correctBillObject.title
+      );
+      assert.equal(
+        openerr.testCreateBillObject(sampleBillData).latestAction,
+        correctBillObject.latestAction
+      );
+      assert.equal(
+        openerr.testCreateBillObject(sampleBillData).latestActionDate,
+        correctBillObject.latestActionDate
+      );
+      assert.equal(
+        openerr.testCreateBillObject(sampleBillData).openstatesUrl,
+        correctBillObject.openstatesUrl
+      );
+    });
+  });
+  describe('#startTweeting()', function() {
+    it('should return false when bills are empty', function() {
+      // We should receive an email about this as well
+      assert.equal(openerr.testStartTweeting([]), false);
+    });
+    it('should return the bills when they are present', function() {
+      assert.equal(openerr.testStartTweeting([correctBillObject]).length, 1);
+    });
+  });
+});
